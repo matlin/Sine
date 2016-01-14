044c8b4fa2dac50e9a4cad2c4d02d260b6492023
Matthew Linkous <mlinkous@interapthq.com>
2016-01-14 13:58:38 -0500

    Small fix

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -23,6 +23,7 @@ angular.module("sineApp").controller("Login", ['$scope', '$rootScope' , '$locati
             user.signUp(null, {
               success: function(user) {
                 // Hooray! Let them use the app now.
+                 console.log("Added user.", user);
               },
               error: function(user, error) {
                 // Show the error message somewhere and let the user try again.
